064b1f5b47d2e1bae3b90e8f144b5382da59e2ad
Troy Feng <troyfeng1@gmail.com>
2023-11-16 22:17:44 -0500

    Hooks file structure

--- src/hooks/usePets/index.ts
+++ src/hooks/usePets/index.ts
@@ -5,32 +5,43 @@ import { Pet } from 'models/Pet'
 import { PETS_ERROR, PETS_LOADING, PETS_SUCCESS } from './actions'
 import { reducer } from './reducer'
 
-export interface PetsHookState {
+export interface UsePetsState {
     isLoading: boolean
     error?: string
     lastUpdated?: Date
     pets?: Pet[]
 }
 
-interface PetsHook {
-    petsState: PetsHookState
-
-    triggerUpdate: () => void
-}
-
-const initialPetsHookState: PetsHookState = {
+const initialPetsHookState: UsePetsState = {
     isLoading: true,
     error: undefined,
     lastUpdated: undefined,
     pets: undefined,
 }
 
-export const usePets = (): PetsHook => {
+interface UsePetsHook {
+    petsState: UsePetsState
+
+    triggerUpdate: () => void
+}
+
+/**
+ * Custom hook to manage pet info fetching.
+ *
+ * @returns
+ * - `petsState`: fetch status and payload.
+ * - `triggerUpdate`: invocation triggers fresh update.
+ */
+export const usePets = (): UsePetsHook => {
     const [state, dispatch] = useReducer(reducer, initialPetsHookState)
     const [shouldTriggerUpdate, setShouldTriggerUpdate] = useState<boolean>(true)
+    const { isLoading } = state
 
+    // TODO: throttle updates? SWR?
     const triggerUpdate = () => {
-        setShouldTriggerUpdate((prevShouldTriggerUpdate) => !prevShouldTriggerUpdate)
+        if (!isLoading) {
+            setShouldTriggerUpdate((prevShouldTriggerUpdate) => !prevShouldTriggerUpdate)
+        }
     }
 
     useEffect(() => {
--- src/hooks/usePets/reducer.ts
+++ src/hooks/usePets/reducer.ts
@@ -1,7 +1,7 @@
 import { PETS_ERROR, PETS_LOADING, PETS_SUCCESS, UsePetsAction } from './actions'
-import { PetsHookState } from './usePets'
+import { UsePetsState } from '.'
 
-export const reducer = (state: PetsHookState, action: UsePetsAction): PetsHookState => {
+export const reducer = (state: UsePetsState, action: UsePetsAction): UsePetsState => {
     switch (action.type) {
         case PETS_LOADING:
             return { ...state, isLoading: true, error: undefined }
